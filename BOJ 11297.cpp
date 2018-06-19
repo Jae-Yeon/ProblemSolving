d0afeaeb62d1ee3cd50344ff7f9b2fab99deaa77
JaeYeon Kim <38181067+Jae-Yeon@users.noreply.github.com>
2018-06-19 09:55:03 +0900

    Update BOJ 11297.cpp

--- BOJ 11297.cpp
+++ BOJ 11297.cpp
@@ -3,23 +3,27 @@
 #include<queue>
 using namespace std;
 //백준 온라인 저지 11297번 최대 힙
-int n;
-priority_queue<int> pq;
+int n; // 연산의 개수를 받아줄 변수
+priority_queue<int> pq; //int형 우선순위 큐 선언
 int main() {
 	cin >> n;
-	while (n--) {
-		int m;
-		cin >> m;
-		if (m == 0) {
+	while (n--) { //연산의 개수만큼 반복
+		int x; //연산에 대한 정보를 받아줄 변수
+		cin >> x;
+		if (x == 0) { //x가 0이라면
 			if (pq.empty()) cout << "0\n";
+			//queue가 비어있을 경우 0 출력
 			else {
 				cout << pq.top() << "\n";
 				pq.pop();
+				//가장 위에 들어가있는 값을 출력
+				//pop()을 해주면 queue의 FIFO 성질 때문에 맨 위의 값이 나오게 된다.
 			}
 		}
-		else {
-			pq.push(m);
+		else { //x가 0이 아니라면
+			pq.push(x); //입력한 숫자 queue에 삽입
 		}
 	}
 	return 0;
 }
+
