4b2c42eed2c0f76489aad40d67ddb22452e7781d
JaeYeon Kim <38181067+Jae-Yeon@users.noreply.github.com>
2018-06-24 23:20:27 +0900

    Create BOJ 1152.java

---
+++ BOJ 1152.java
@@ -0,0 +1,15 @@
+//백준 온라인 저지 1152번 단어의 개수
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main{
+   public static void main (String[] args) 
+   {
+	   Scanner sc = new Scanner(System.in);
+	   String a = sc.nextLine();
+	   StringTokenizer st = new StringTokenizer(a);
+	   System.out.print(st.countTokens());	   
+	 }
+	   
+}
