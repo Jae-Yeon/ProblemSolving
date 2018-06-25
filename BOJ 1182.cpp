e5685f4386171cbda95709d9add09070b56ff01d
JaeYeon Kim <38181067+Jae-Yeon@users.noreply.github.com>
2018-06-25 10:05:19 +0900

    Create BOJ 1182.cpp

---
+++ BOJ 1182.cpp
@@ -0,0 +1,15 @@
+//백준 온라인 저지 1182번 
+#include<stdio.h>
+int n, s, sum, ans, a[22];
+int main() {
+	scanf("%d %d", &n,&s);
+	for (int i = 0; i < n; i++) scanf("%d", &a[i]);
+	for (int i = 1; i < (1 << n); i++) {
+		sum = 0;
+		for (int j = 0; j < n; j++) if (i&(1 << j)) sum += a[j];
+		ans += (s == sum);
+	}
+	printf("%d", ans);
+
+	return 0;
+}
